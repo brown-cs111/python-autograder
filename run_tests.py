1cb44a127abf92c4699d3b492d1848ae56c6484f
colton <51266557+coltonrusch@users.noreply.github.com>
2020-08-25 03:09:25 -0400

    Fixed testsuite discovery

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,6 @@ import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('/autograder/source/tests')
+    suite = unittest.defaultTestLoader.discover('/autograder/source/instructor/tests')
     with open('/autograder/results/results.json', 'w') as f:
         JSONTestRunner(visibility='visible', stream=f).run(suite)
