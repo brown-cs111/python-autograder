4ca37c06903d70ebdabdb9773dd639ba0536df36
colton <51266557+coltonrusch@users.noreply.github.com>
2020-08-25 03:05:33 -0400

    Updated the test suite location

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,6 @@ import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('../tests')
+    suite = unittest.defaultTestLoader.discover('/autograder/source/tests')
     with open('/autograder/results/results.json', 'w') as f:
         JSONTestRunner(visibility='visible', stream=f).run(suite)
