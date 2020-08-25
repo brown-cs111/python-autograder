8704825278704a9d5d7839a6014c59b77c6dea2a
colton <51266557+coltonrusch@users.noreply.github.com>
2020-08-25 04:48:52 -0400

    Python3 installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-apt-get install -y python3.8 python3-pip python3-dev
+add-apt-repository ppa:deadsnakes/ppa   
+apt-get update   
+apt-get install -y python3.7 python3-pip python3-dev
 
 pip3 install -r /autograder/source/python-autograder/requirements.txt
