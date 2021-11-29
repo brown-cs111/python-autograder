2a42b2c9a4574e9085fcc919ff1998b7b40bd933
Ryan Simpson <ryansimpson@ryans-mbp.devices.brown.edu>
2021-11-29 17:46:24 -0500

    post processor 2

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 if __name__ == '__main__':
 
     def handle_json(json):
-        print(json)
+        pass
 
     suite = unittest.defaultTestLoader.discover('/autograder/source/instructor/tests')
     with open('/autograder/results/results.json', 'w') as f:
