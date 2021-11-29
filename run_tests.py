bb9cd8e30e88cecae1d06eea0a2720af64ed4d4e
Ryan Simpson <ryansimpson@ryans-mbp.devices.brown.edu>
2021-11-29 17:37:39 -0500

    post_processor

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,10 @@ import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
+
+    def handle_json(json):
+        print(json)
+
     suite = unittest.defaultTestLoader.discover('/autograder/source/instructor/tests')
     with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(visibility='visible', stream=f, buffer=False).run(suite)
+        JSONTestRunner(visibility='visible', stream=f, post_processor=handle_json).run(suite)
