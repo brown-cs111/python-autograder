23a733b1c9e5c8c99c29744d5d87b37b880e856e
Ryan Simpson <ryansimpson@ryans-mbp.devices.brown.edu>
2021-11-29 17:32:23 -0500

    fix visibility

--- run_tests.py
+++ run_tests.py
@@ -4,4 +4,4 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('/autograder/source/instructor/tests')
     with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(stream=f, buffer=False).run(suite)
+        JSONTestRunner(visibility='visible', stream=f, buffer=False).run(suite)
